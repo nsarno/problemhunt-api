ce6c8a22bb167f0caed9277ab1460b3f281ceb3d
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2014-10-26 10:48:59 +0100

    fixed specs

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ProblemsController, :type => :controller do
 
+  before(:each) do
+    @current_user = create :user
+    controller.stub(:authenticate) { @current_user }
+  end
+
   describe "GET index" do
+    it 'requires authentication' do
+      controller.unstub(:authenticate)
+      get :index
+      expect(response.status).to eq(401)
+    end
+
     it "responds with success" do
       get :index
       expect(response.status).to be(200)
